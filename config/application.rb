a457000099cdf81e1f2059584fdac40a3ebdf98f
saitotm <tomokazu.ml.tech@gmail.com>
2021-04-05 15:17:14 +0900

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = ENV["TZ"] # added
+    config.active_record.default_timezone = :utc # added
+    config.i18n.default_locale = :ja # added
+    config.add_autoload_paths_to_load_path = false #added
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
